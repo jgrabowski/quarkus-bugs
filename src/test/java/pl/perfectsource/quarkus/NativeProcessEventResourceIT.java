1143d820c2b50fae85230a6981a6ebe03bb88c10
Jakub Grabowski <jgrabowski@openbs.pl>
2021-03-04 18:03:40 +0100

    Initial import.

---
+++ src/test/java/pl/perfectsource/quarkus/NativeProcessEventResourceIT.java
@@ -0,0 +1,9 @@
+package pl.perfectsource.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeProcessEventResourceIT extends ProcessEventResourceTest {
+
+    // Execute the same tests but in native mode.
+}
